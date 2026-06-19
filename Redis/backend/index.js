d19b70f18fa43cd6bbad7399bff4d4c7a6cfc13e
nakulgadhewal050 <nakulgadhewal050@gmail.com>
2026-06-19 12:13:40 +0530

    redis implimentation

--- Redis/backend/index.js
+++ Redis/backend/index.js
@@ -4,9 +4,45 @@ import dotenv from "dotenv";
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 import connectDB from "./lib/db.js";
+import dns from "dns";
+import User from "./model/userModel.js";
+import Redis from "ioredis";
+
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+app.use(express.json())
+
+const redis = new Redis(process.env.REDIS_URL);
 
 app.get("/", (req, res) => {
     res.send("Hello World");
+})
+
+app.post("/create", async (req, res) => {
+  const { name, email, password } = req.body;
+  const user = await User.create({
+    name, email, password
+  })
+  return res.status(201).json(user)
+})
+
+app.get("/user", async (req, res) => {
+  const user = await User.find({})
+
+  return res.status(201).json(user)
+})
+
+app.get("/redis", async (req, res) => {
+    const cachedData = await redis.get("user:all")
+    if(cachedData){
+        return res.json(JSON.parse(cachedData))
+    }
+
+  const user = await User.find({})
+  await redis.set("user:all", JSON.stringify(user), "EX", 10) 
+
+  return res.json(user)
 })
 
 app.listen(PORT, () => {
